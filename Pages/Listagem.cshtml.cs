26f30f24458e0256b01b0f16b004e9f0e5ccc6b9
Leonardo Silva <leonardoferreira032@gmail.com>
2022-12-28 20:24:48 -0300

    Criação do projeto e organização de páginas

---
+++ Pages/Listagem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web.Pages
+{
+    public class ListagemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
